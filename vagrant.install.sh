ba78e360a7b29b50906da92d7074574f355afff5
Dmitry <dmitry.vaghin@cybera.ca>
2015-10-17 21:46:31 +0000

    vagrant box installation command added

--- vagrant.install.sh
+++ vagrant.install.sh
@@ -3,4 +3,6 @@
 wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.4_x86_64.deb
 dpkg -i vagrant_1.7.4_x86_64.deb 
 vagrant plugin install vagrant-openstack-plugin
-
+vagrant box add dummy https://github.com/cloudbau/vagrant-openstack-plugin/raw/master/dummy.box
+#vagrant box install dummy
+#vagrant box add ubuntu/trusty64
