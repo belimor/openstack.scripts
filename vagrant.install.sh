70f482ba1c3ddee1f86fce16a51b1736ebedb2a5
Dmitry <dmitry.vaghin@cybera.ca>
2015-10-27 23:20:39 +0000

    vagrant kvm support added (commented out) + kvm packages installation

--- vagrant.install.sh
+++ vagrant.install.sh
@@ -4,5 +4,25 @@ wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.4_x86_64.deb
 dpkg -i vagrant_1.7.4_x86_64.deb 
 vagrant plugin install vagrant-openstack-plugin
 vagrant box add dummy https://github.com/cloudbau/vagrant-openstack-plugin/raw/master/dummy.box
-#vagrant box install dummy
+
+
+## vagrant kvm plugins and kvm packages
+#apt-get install cpu-checker
+#echo -e "/n"
+#echo "checking virtualisation support"
+#kvm-ok
+#egrep -c '(vmx|svm)' /proc/cpuinfo
+#echo -e "/n"
+#sleep 4
+#
+#apt-get install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils
+#apt-get install libvirt-dev
+#apt-get install virt-manager
+#
+#vagrant plugin install vagrant-libvirt
+#vagrant plugin install vagrant-mutate
 #vagrant box add ubuntu/trusty64
+#vagrant mutate ubuntu/trusty64 libvirt
+#vagrant box list
+#
+#vagrant up --provider=libvirt
